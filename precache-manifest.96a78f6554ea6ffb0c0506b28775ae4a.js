def9e091b6ddbe3f5c576cb15eb5bfd1985b9f9d
logon pierce <logonpierce@gmail.com>
2021-09-17 00:22:38 -0400

    Updates

---
+++ precache-manifest.96a78f6554ea6ffb0c0506b28775ae4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57585329ba607b320b301bc7aa10098a",
+    "url": "/wk20-react-portfolio/index.html"
+  },
+  {
+    "revision": "c8ccb0f4d5f203c16eea",
+    "url": "/wk20-react-portfolio/static/css/main.883912da.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/wk20-react-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/wk20-react-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ccb0f4d5f203c16eea",
+    "url": "/wk20-react-portfolio/static/js/main.8b705ba8.chunk.js"
+  },
+  {
+    "revision": "39bbdf27fec9f9c47ac4",
+    "url": "/wk20-react-portfolio/static/js/runtime-main.59e54171.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/wk20-react-portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/wk20-react-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/wk20-react-portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/wk20-react-portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/wk20-react-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/wk20-react-portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
